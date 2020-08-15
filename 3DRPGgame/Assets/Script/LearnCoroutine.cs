58fb1bdb1ce63bae69ee3302cf7ff28871a98ff2
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-08-15 19:50:09 +0800

    練習程式

---
+++ 3DRPGgame/Assets/Script/LearnCoroutine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LearnCoroutine : MonoBehaviour
+{
+    public Transform ming;
+
+    private void Start()
+    {
+        StartCoroutine(Test());
+        StartCoroutine(Big());
+    }
+
+    public IEnumerator Test()
+    {
+        print("嗨，我是協程");
+        yield return new WaitForSeconds(2);
+        print("嗨，我試過了兩秒的協程");
+
+    }
+
+    public IEnumerator Big()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ming.localScale += Vector3.one;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ 3DRPGgame/Assets/Script/MenuMenger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MenuMenger : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+        Application.Quit();
+    } 
+
+
+}
